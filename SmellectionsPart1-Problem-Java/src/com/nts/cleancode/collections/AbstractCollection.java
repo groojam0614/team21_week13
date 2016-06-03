1b5e21143f238c964e0fdab460d520bc6eb9d3aa
groojam0614 <ayfls0614@naver.com>
2016-06-03 23:23:14 +0900

    Modify Map.java & AbstractCollection
    Modify Map.java
    Modify AbstractCollection.Java

--- SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/AbstractCollection.java
+++ SmellectionsPart1-Problem-Java/src/com/nts/cleancode/collections/AbstractCollection.java
@@ -17,10 +17,6 @@ public abstract class AbstractCollection  {
 					add(l.get(i));
 				}
 			}
-		} else if (c instanceof Map) {
-			Map m = (Map)c;
-			for (int i=0; i<m.size(); i++) 
-				add(m.keys[i], m.values[i]);			
 		}
 	}
 	
@@ -30,6 +26,4 @@ public abstract class AbstractCollection  {
 	public abstract boolean contains(Object element);
 	public abstract int size();
 	
-	public void add(Object key, Object value) {
-	}
 }
